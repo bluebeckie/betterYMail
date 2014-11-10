062ae6b0043ef9853ca25251bd35ca33ddefe559
beckie <bluebeckie@gmail.com>
2014-11-10 23:21:28 +0800

    improve screen real estate
    - remove top nav
    - remove ads
    - auto-expand folders
    - highlight current folder

---
+++ betterymail.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         better YMail
+// @namespace    http://github.com/bluebeckie/betterYMail
+// @version      0.1
+// @description  more space
+// @author       beckie
+// @match            https://*.mail.yahoo.com/*
+// @grant        none
+// ==/UserScript==
+
+var subfolders,
+    style,
+    styles = [
+    '#UH {padding-top:10px; height:4em;}',
+    '#masterNav {display: none;}',
+    '.wide-right-rail #shellcontent, #shellcontent {right:0;}',
+    '.wide-right-rail #theAd, #theAd {width: 0;}',
+    '#slot_MB {display:none;}',
+    '#masSearchAd {display: none;}',
+    '.listnav .selected { background: deepskyblue;}',
+    '.has-tabs #main {top: 60px;}',
+    '.yui3-feedback .expand .feedback-content {display:none;}'
+].join('');
+
+style = document.createElement('style');
+style.innerHTML = styles;
+document.head.appendChild(style);
+
+subfolders = document.querySelectorAll('.listnav-subfolder.hidden');
+for (var i=0,j=subfolders.length; i<j; i++) {
+     subfolders[i].className = subfolders[i].className.replace('hidden','');
+};
