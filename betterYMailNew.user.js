047218dbb691fcafc9fd1c5a6a1b1e5aafca5fbc
beckie <bluebeckie@gmail.com>
2016-07-28 12:10:52 +0800

    remove ads for yo/newmail

--- betterYMailNew.user.js
+++ betterYMailNew.user.js
@@ -4,13 +4,14 @@
 // @version      1.0
 // @description  remove ads from yo/newmail
 // @author       beckie
-// @match        http://*.mail.yahoo.com/*
+// @match        https://*.mail.yahoo.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     const styles = [
+        '#Atom .P_6Fd5.W_3o4BO {padding:0; width:0;}',
         '@media (min-width: 1360px) { #Atom .P_6Fd5.W_3o4BO.cZ10Gnkk_3vpWV {overflow:hidden; padding:0; width:0;}}',
         '@media (min-width: 1440px) { #Atom .P_6Fd5.W_3o4BO.cZ10Gnkk_3vpWV {overflow:hidden; padding:0; width:0;}}',
         '#Atom .p_f {display: none;}'
